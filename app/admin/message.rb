b88b3c56b9b32a0bfd82662ff135610f9576335e
Frank Vinci <fv42wid@yahoo.com>
2017-09-10 20:01:57 -0400

    allowed admin to update message, stripe next

--- app/admin/message.rb
+++ app/admin/message.rb
@@ -11,5 +11,6 @@ ActiveAdmin.register Message do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+  permit_params :name, :email, :body, :responded_to
 
 end
